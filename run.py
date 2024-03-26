b60aa43887e76c645b72edbe6d88ad4e970ed85d
isiz <n0749086@gmail.com>
2024-03-26 23:31:22 +0900

    configファイルを読み込み、そのURLをfeedを取得する処理の追加

--- run.py
+++ run.py
@@ -1,11 +1,26 @@
+from argparse import ArgumentParser
+from pathlib import Path
 
+import config
 import setting
 
-import google.generativeai as genai
 import feedparser
+import google.generativeai as genai
+
+
+def parse_args():
+	parser = ArgumentParser()
+	parser.add_argument('config_path', type=Path)
+	return parser.parse_args()
 
 def main():
-	pass
+	args = parse_args()
+	list_target = config.load_config(args.config_path)
+	
+	for url, category in list_target:
+		d_atom = feedparser.parse(url)
+		for elem in d_atom['entries']:
+			print(elem['title'], elem['summary'])
 
 if __name__ == '__main__':
 	main()
